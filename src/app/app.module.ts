245848632effbe5387feee39f51c8e3e29e8bd55
Felix Baertschi <baertschi91@gmail.com>
2019-01-07 15:46:24 +0100

    Add Single

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { SingleComponent } from './single/single.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    SingleComponent,
   ],
   imports: [
     BrowserModule,
